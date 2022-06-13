e79c63b6a1fbfbfa2ff5c9abf8195f76b41d9e2c
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-13 15:42:27 -0500

    saving bonus for Methods exercise 1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,44 +1,68 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MethodsExercises {
+
+    static Scanner scan = new Scanner(System.in);
 
     //Visibility Identifier(public), static keyword (method belongs class), returnType, nameOfMethod, parameters;
     //Return String concat
-    public static String kudos (String name) {
+    public static String kudos(String name) {
         return "Great Job " + name + "!";
     }
+
     //Return addition
-    public static Integer addition(int num1, int num2){
+    public static Integer addition(int num1, int num2) {
         return num1 + num2;
     }
+
     //Return subtraction
-    public static Integer subtraction(int num1, int num2){
+    public static Integer subtraction(int num1, int num2) {
         return num1 - num2;
     }
+
     //Return Multiplication
-    public static Integer multiplication(int num1, int num2){
+    public static Integer multiplication(int num1, int num2) {
         return num1 * num2;
     }
+
     //Return Division
-    public static Integer division(int num1, int num2){
+    public static Integer division(int num1, int num2) {
         return num1 / num2;
     }
+
     //Returns Modulus
-    public static Integer modulus(int num1, int num2){
+    public static Integer modulus(int num1, int num2) {
         return num1 % num2;
     }
+
     //Returns Recursion Multiplication
-    public static Integer recursion(int num1, int num2){
+    public static Integer recursion(int num1, int num2) {
         if (num1 == 0 || num2 == 0) {
             return 0;
-        }
-
-        else if( num2 < 0 ) {
-            return - num1 + recursion(num1, num2 + 1);
-        }
-
-        else {
+        } else if (num2 < 0) {
+            return -num1 + recursion(num1, num2 + 1);
+        } else {
             return num1 + recursion(num1, num2 - 1);
         }
     }
+
+    //Return User Input in certain range
+    public static void range(int min, int max) {
+        System.out.printf("Give me a number between %d and %d", min, max);
+        int userInput = scan.nextInt();
+        if (userInput < min || userInput > max) {
+            System.out.printf("GIVE ME A VALID NUMBER BETWEEN %d and %d%n", min, max);
+        } else if (userInput >= min && userInput <= max) {
+            System.out.printf("That is a valid number: %d%n", userInput);
+        }
+        System.out.println("Do you want to enter another number? (Y/N)");
+        String userChoice = scan.next();
+        if (userChoice.equalsIgnoreCase("y")) {
+            range(min, max);
+        }
+    }
+
 
     public static void main(String[] args) {
 
@@ -61,7 +85,10 @@ public class MethodsExercises {
         System.out.println(modulus(100, 2));
 
         //Recursion to Multiply
-        System.out.println(recursion(10, 2));
+        System.out.println(recursion(10, 10));
+
+        //Get integer in this range
+        range(30, 47);
 
 
     }
