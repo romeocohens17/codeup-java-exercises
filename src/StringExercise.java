9ec70a3ecc4f59f05a9e8a0057bcee8fee7fc0ef
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-13 11:29:27 -0500

    saving string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+//        System.out.println(new Object());
+//        System.out.println(new Object());
+//
+//        String str1 = "Hello";
+//        String str2 = "hello";
+//
+//        System.out.println(str1.equalsIgnoreCase(str2));
+//
+//        str1 = "World!";
+//        System.out.println(str1.toLowerCase().startsWith("w"));
+//        System.out.println(str1.toLowerCase().endsWith("!"));
+//
+//        String testString = "This is a test string! Please don't pay this Test no mind!";
+//
+//        //String Methods
+//        System.out.println(testString.charAt(8));
+//        System.out.println(testString.indexOf("pay"));
+//        //Myst use parenthesis for .length()
+//        System.out.println(testString.length());
+//        System.out.println(testString.toLowerCase().replace("test", "word"));
+
+
+        //Strings Exercise
+
+        System.out.println("We don't need no education\n" +
+                "We don't need no thought control");
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println("In windows, the main drive is usually C:\\");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!");
+
+
+
+    }
+}
