1c8a450c9e696586577eda58e7f1a04f1ad2abc9
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-13 12:20:37 -0500

    saving work

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,14 @@
 public class MethodsExercises {
+
+    //Visibility Identifier(public), static keyword (method belongs class), returnType, nameOfMethod, parameters;
+
+    public static String kudos (String name) {
+        return "Great Job " + name + "!";
+    }
+
     public static void main(String[] args) {
 
-        //Methods
-
-
-
-
+        System.out.println(kudos("Romeo"));
 
 
     }
---
+++ src/MethodsPractice.java
@@ -0,0 +1,12 @@
+public class MethodsPractice {
+
+    public static Integer getDifference (int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getDifference(28, 11));
+
+    }
+}
