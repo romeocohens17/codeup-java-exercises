32d51d0bc2755b0f224c99eeb84f891b2c3162e0
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-16 11:02:49 -0500

    saving person class

--- src/person.java
+++ src/person.java
@@ -2,25 +2,28 @@ public class person {
    public String name;
    public int age;
    public String occupation;
+   public String home;
 
     public String sayHello() {
-        return String.format("Hello, my name is %s, and I am %d years old. I am a %s.", this.name, this.age, this.occupation);
+        return String.format("Hello, my name is %s, and I am %d years old. I am from %s and I am a %s.", this.name, this.age, this.home, this.occupation);
     }
 
-    public person(String name, int age, String occupation) {
+    public person(String name, int age,String home, String occupation) {
         this.name = name;
         this.age = age;
         this.occupation = occupation;
+        this.home = home;
     }
 
     public person(){
     }
 
     public static void main(String[] args) {
-        person romeo = new person("Romeo", 24, "Linguist/Software Engineer");
+        person romeo = new person("Romeo", 24, "Washington State", "Linguist/Software Engineer");
 //        romeo.name = "Romeo";
 //        romeo.age = 24;
 //        romeo.occupation = "Linguist/Software Engineer";
+//          romeo.home = "Washington State"
 
         System.out.println(romeo.sayHello());
     }
