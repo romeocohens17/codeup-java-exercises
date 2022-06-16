7611a3e81c11b302517c960e22a9f375f07702e5
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-16 14:50:07 -0500

    saving work

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,33 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getString() {
+        System.out.println(("What is your name?\n"));
+        String name = scanner.nextLine();
+        return name;
+    }
+
+    public boolean yesNo() {
+        System.out.println("Enter Yes or No: (Y/N)");
+        String resp = scanner.nextLine();
+        boolean decision = false;
+        if (resp.equalsIgnoreCase("y")) {
+            decision = true;
+        }
+        return decision;
+    }
+
+
+    public static void main(String[] args) {
+        Input name = new Input();
+        System.out.println(name.getString());
+
+        Input call = new Input();
+        System.out.println(call.yesNo());
+
+    }
 }
