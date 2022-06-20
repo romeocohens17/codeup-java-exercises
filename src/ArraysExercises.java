fdd75d190f66ac0e97894862185881c12dfa8474
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-20 09:40:32 -0500

    saving work

---
+++ src/ArraysExercises.java
@@ -0,0 +1,30 @@
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+
+//Two Dimensional Arrays Example from Code up Curriculum
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int[] row : matrix) {
+            System.out.println("+---+---+---+");
+
+            System.out.print("| ");
+
+            for (int n : row) {
+                System.out.print(n + " | ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("+---+---+---+");
+
+
+
+    }
+
+}
