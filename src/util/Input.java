8e9ebca58c2201bdac1c7765ec476118d0c66745
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-20 09:09:23 -0500

    saving work

--- src/util/Input.java
+++ src/util/Input.java
@@ -21,6 +21,42 @@ public class Input {
         return decision;
     }
 
+    public Integer getInt(int min, int max) {
+        System.out.printf("Give me a number between %d and %d.", min, max);
+        int input = scanner.nextInt();
+        if (input <= min || input >= max) {
+            System.out.println("Sorry, wrong answer");
+            getInt(min, max);
+        }else if(input >= min && input <= max){
+            System.out.println("Your number was: ");
+        }
+        return input;
+    }
+
+    public Integer getInt() {
+        System.out.println("What is your favorite number?");
+        int favorite = scanner.nextInt();
+        return favorite;
+    }
+
+    public Double getDouble(double min, double max) {
+        System.out.printf("Give me a number between %.0f and %.0f.", min, max);
+        double input = scanner.nextDouble();
+        if (input <= min || input >= max) {
+            System.out.println("Sorry, wrong answer");
+            getDouble(min, max);
+        }else if(input >= min && input <= max){
+            System.out.println("Your number was: ");
+        }
+        return input;
+    }
+
+    public Double getDouble() {
+        System.out.println("What is your favorite number?");
+        double favorite = scanner.nextDouble();
+        return favorite;
+    }
+
 
     public static void main(String[] args) {
         Input name = new Input();
@@ -28,6 +64,18 @@ public class Input {
 
         Input call = new Input();
         System.out.println(call.yesNo());
+
+        Input minMax = new Input();
+        System.out.println(minMax.getInt(1, 100));
+
+        Input favoriteNumber = new Input();
+        System.out.println(favoriteNumber.getInt());
+
+        Input doubleMinMax = new Input();
+        System.out.println(doubleMinMax.getDouble(1, 100));
+
+        Input doubling = new Input();
+        System.out.println(doubling.getDouble());
 
     }
 }
