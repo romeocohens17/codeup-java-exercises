847f076cddb9b3aa2ddb8edf128995632e6fb269
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-21 14:13:14 -0500

    saving work

--- src/Udemy/Main.java
+++ src/Udemy/Main.java
@@ -3,13 +3,22 @@ package Udemy;
 public class Main {
 
     public static void main(String[] args) {
-       Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        //Inheritance and Polymorphism
+//       Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//
+//       Dog dog = new Dog("Yorkie", 8,20, 2, 4, 1, 20, "Long Silky");
+//       dog.eat();
+//       dog.walk();
+//       dog.run();
 
-       Dog dog = new Dog("Yorkie", 8,20, 2, 4, 1, 20, "Long Silky");
-       dog.eat();
-       dog.walk();
-       dog.run();
-
+       //Collections
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+//        theatre.getSeats();
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please Pay");
+        } else {
+            System.out.println("Sorry, seat is taken!");
+        }
 
 
 
