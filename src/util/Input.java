866753c4249fe683a93c75c7546d783c36cb4840
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-27 16:41:03 -0500

    saving input class updated with try and catch

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,14 +1,17 @@
 package util;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class Input {
     private Scanner scanner = new Scanner(System.in);
 
     public String getString() {
-        System.out.println(("What is your name?\n"));
-        String name = scanner.nextLine();
-        return name;
+//        System.out.println(("What is your name?\n"));
+        String string = scanner.nextLine();
+        return string;
     }
 
     public boolean yesNo() {
@@ -21,61 +24,85 @@ public class Input {
         return decision;
     }
 
+
     public Integer getInt(int min, int max) {
-        System.out.printf("Give me a number between %d and %d.", min, max);
-        int input = scanner.nextInt();
-        if (input <= min || input >= max) {
-            System.out.println("Sorry, wrong answer");
-            getInt(min, max);
-        }else if(input >= min && input <= max){
-            System.out.println("Your number was: ");
+        System.out.printf("Give me a number between %d and %d.%n", min, max);
+        Integer input;
+        try {
+            input = Integer.valueOf(getString());
+            if (input <= min || input >= max) {
+                System.out.println("Sorry, wrong answer");
+                return getInt(min, max);
+            } else if (input >= min && input <= max) {
+                System.out.println("Your number was: ");
+            }
+            return input;
+        } catch (NumberFormatException e) {
+            System.out.println("That's not right!");
+            return getInt(min, max);
         }
-        return input;
     }
 
-    public Integer getInt() {
-        System.out.println("What is your favorite number?");
-        int favorite = scanner.nextInt();
-        return favorite;
-    }
-
-    public Double getDouble(double min, double max) {
-        System.out.printf("Give me a number between %.0f and %.0f.", min, max);
-        double input = scanner.nextDouble();
-        if (input <= min || input >= max) {
-            System.out.println("Sorry, wrong answer");
-            getDouble(min, max);
-        }else if(input >= min && input <= max){
-            System.out.println("Your number was: ");
+        public Integer getInt () {
+            System.out.println("What is your favorite number?");
+            int favorite;
+            try {
+                favorite = Integer.valueOf(getString());
+                System.out.println("Your favorite number is: ");
+                return favorite;
+            } catch (NumberFormatException e) {
+                return getInt();
+            }
         }
-        return input;
+
+        public Double getDouble ( double min, double max){
+            System.out.printf("Give me a number between %.0f and %.0f.%n", min, max);
+            double input;
+            try {
+                input = Double.valueOf(getString());
+                if (input < min || input > max) {
+                    System.out.println("Sorry, wrong answer");
+                    return getDouble(min, max);
+                } else if (input >= min && input <= max) {
+                    System.out.println("Your number was: ");
+                }
+                return input;
+            } catch (NumberFormatException e) {
+                return getDouble();
+            }
+        }
+
+        public Double getDouble () {
+            System.out.println("What is your favorite number?%n");
+            double favorite;
+            try {
+                favorite = Double.valueOf(getString());
+                System.out.println("Your favorite number is: ");
+                return favorite;
+            } catch (NumberFormatException e) {
+                return getDouble();
+            }
+        }
+
+
+        public static void main (String[]args){
+//        Input name = new Input();
+//        System.out.println(name.getString());
+//
+//        Input call = new Input();
+//        System.out.println(call.yesNo());
+
+            Input minMax = new Input();
+            System.out.println(minMax.getInt(1,10));
+
+            Input favoriteNumber = new Input();
+            System.out.println(favoriteNumber.getInt());
+
+            Input doubleMinMax = new Input();
+            System.out.println(doubleMinMax.getDouble(1, 100));
+
+            Input favoriteDouble = new Input();
+            System.out.println(favoriteDouble.getDouble());
+
+        }
     }
-
-    public Double getDouble() {
-        System.out.println("What is your favorite number?");
-        double favorite = scanner.nextDouble();
-        return favorite;
-    }
-
-
-    public static void main(String[] args) {
-        Input name = new Input();
-        System.out.println(name.getString());
-
-        Input call = new Input();
-        System.out.println(call.yesNo());
-
-        Input minMax = new Input();
-        System.out.println(minMax.getInt(1, 100));
-
-        Input favoriteNumber = new Input();
-        System.out.println(favoriteNumber.getInt());
-
-        Input doubleMinMax = new Input();
-        System.out.println(doubleMinMax.getDouble(1, 100));
-
-        Input doubling = new Input();
-        System.out.println(doubling.getDouble());
-
-    }
-}
